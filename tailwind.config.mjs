57169f0fa832e6afefd07bc7442483bbc9e2b8ab
Bruno Gustavo Kannenberg <brunokannenberg794@gmail.com>
2026-07-09 09:19:24 -0300

    Feat: CSS and Tailwind adjustments

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        app: {
+          primary: '#2563eb',
+          dark: '#172033',
+          soft: '#eff6ff'
+        }
+      }
+    },
+  },
+  plugins: [],
+};
